22bf53838814b27e446e75801ac863e3593eb0ae
Michael Lauritsen <i30288741@live.dk>
2018-10-17 08:55:32 +0200

    Observer Method Done

--- Observer Pattern/Program.cs
+++ Observer Pattern/Program.cs
@@ -8,8 +8,94 @@ namespace Observer_Pattern
 {
     class Program
     {
+        abstract class Veggies
+        {
+            private double _PricePerPound;
+            private List<IRestaurant> _restaurants = new List<IRestaurant>();
+
+            public Veggies(double pricePerPound)
+            {
+                _PricePerPound = pricePerPound;
+            }
+
+            public void Attach(IRestaurant restaurant)
+            {
+                _restaurants.Add(restaurant);
+            }
+
+            public void Detach(IRestaurant restaurant)
+            {
+                _restaurants.Remove(restaurant);
+            }
+
+            public void Notify()
+            {
+                foreach (IRestaurant restaurant in _restaurants)
+                {
+                    restaurant.Update(this);
+                }
+
+                Console.WriteLine("");
+            }
+            public double PricePerPound
+            {
+                get { return _PricePerPound; }
+                set
+                {
+                    if (_PricePerPound != value)
+                    {
+                        _PricePerPound = value;
+                        Notify(); // Automatically notify observers of price changes
+                    }
+                }
+            }
+        }
+
+        class Carrots : Veggies
+        {
+            public Carrots (double price) : base(price) { }
+        }
+
+        interface IRestaurant
+        {
+            void Update(Veggies veggies);
+        }
+
+        class Restaurant : IRestaurant
+        {
+            private string _name;
+            private Veggies _veggies;
+            private double _purchaseThreshold;
+
+            public Restaurant(string name, double purchaseThreshold)
+            {
+                _name = name;
+                _purchaseThreshold = purchaseThreshold;
+            }
+
+            public void Update(Veggies veggie)
+            {
+                Console.WriteLine(
+                    "Notified {0} of {1}'s " + " price change to {2:C} per pound.",
+                    _name, veggie.GetType().Name, veggie.PricePerPound);
+                if (veggie.PricePerPound < _purchaseThreshold)
+                {
+                    Console.WriteLine(_name + " wants to buy some " + veggie.GetType().Name + "!");
+                }
+            }
+        }
         static void Main(string[] args)
         {
+            Carrots carrots = new Carrots(0.82);
+            carrots.Attach(new Restaurant("Mackay's", 0.77));
+            carrots.Attach(new Restaurant("Johnny's Sports Bar", 0.74));
+            carrots.Attach(new Restaurant("Salad Kingdom", 0.75));
+            // Fluctuating carrot prices will notify subscribing restaurants.
+            carrots.PricePerPound = 0.79;
+            carrots.PricePerPound = 0.76;
+            carrots.PricePerPound = 0.74;
+            carrots.PricePerPound = 0.81;
+            Console.ReadKey();
         }
     }
 }
